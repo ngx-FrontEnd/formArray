2fb53ecff6341556626ce3be38272c668da792a6
VigneshGetbiz <vignesh@getbiz.app>
2021-10-20 16:30:11 +0530

    dynamic add fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formArray';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      arr: this.fb.array([])
+    })
+    this.setField();
+  }
+
+  arrayValue = (): FormArray => {
+    return this.form.get('arr') as FormArray;
+  }
+
+  setField = () => {
+    return (this.form.get("arr") as FormArray).push(this.fb.group({
+      name: [''],
+      company: [''],
+    }));
+
+  }
+  addNewCompany(): any {
+    return (this.form.get("arr") as FormArray).push(this.fb.group({
+      name: [''],
+      company: [''],
+    }))
+
+  }
+
+  deleteCompany(index: any) {
+    let control = <FormArray>this.form.controls.arr;
+    control.removeAt(index)
+  }
+
+
 }
